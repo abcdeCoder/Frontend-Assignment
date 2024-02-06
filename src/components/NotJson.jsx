ad54b2cea00fb1ec9d7be1305fa7ac67504283ea
Gaurav verma <94301843+abcdeCoder@users.noreply.github.com>
2024-02-06 22:35:45 +0530

    Update NotJson.jsx

--- src/components/NotJson.jsx
+++ src/components/NotJson.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { BiError } from "react-icons/bi";
+import { MdDangerous } from "react-icons/md";
 
 const NotJson = () => {
   return (
     <div className="col-span-6 flex flex-col items-center justify-center  h-full">
-      <BiError className="text-6xl scale-150 text-red-500 font-bold" />
+      <MdDangerous className="text-6xl scale-150 text-red-500 font-bold" />
       <p className="text-4xl mt-6 font-bold font-mono">
       Apologies for the confusion, there seems to be an issue with the structure of the JSON schema. Let's refine it to ensure it accurately represents the data model. By improving the schema, we can ensure better clarity and compatibility with the form data. Let's work together to refine and enhance the schema for a smoother form experience
       </p>
